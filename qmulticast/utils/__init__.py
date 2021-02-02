bd4230590cea7a7e22e5d4df3f8a3fa731094499
Michael Williams <ucapiwi@ucl.ac.uk>
2021-02-02 13:42:15 +0000

    changed to networkx graphr

--- qmulticast/utils/__init__.py
+++ qmulticast/utils/__init__.py
@@ -2,11 +2,9 @@
 
 from .bipartite_network import create_bipartite_network
 from .functions import gen_GHZ_ket
-from .graph import Graph
 from .graphlibrary import ButterflyGraph, RepeaterGraph, TwinGraph
 
 __all__ = [
-    Graph,
     ButterflyGraph,
     TwinGraph,
     RepeaterGraph,
--- qmulticast/utils/graphlibrary.py
+++ qmulticast/utils/graphlibrary.py
@@ -1,48 +1,53 @@
 """Defines the butterfly graph"""
 import logging
-
-from qmulticast.utils.graph import Graph
+import networkx as nx
 
 logger = logging.getLogger(__name__)
 
 
-class ButterflyGraph(Graph):
+class ButterflyGraph(nx.DiGraph):
     """Defines the butterfly graph."""
 
     def __init__(self):
         logger.debug("Creating butterfly graph")
-        self.nodes = {5, 1, 2, 3, 4}
-        self.edges = {
+        super().__init__()
+        edges = {
             5: {1: 1, 2: 1},
             1: {5: 1, 2: 1},
             2: {5: 1, 1: 1, 3: 1, 4: 1},
             3: {2: 1, 4: 1},
             4: {2: 1, 3: 1},
         }
+        temp = nx.DiGraph(edges)
+        self.add_edges_from(temp.edges)
 
 
-class TwinGraph(Graph):
+class TwinGraph(nx.DiGraph):
     """Two nodes connected."""
 
     def __init__(self):
+        super().__init__()
         logger.debug("Creating twin graph.")
-        self.nodes = {0, 1}
-        self.edges = {0: {1: 1}, 1: {0: 1}}
+        edges = {0: {1: 1}, 1: {0: 1}}
+        temp = nx.DiGraph(edges)
+        self.add_edges_from(temp.edges)
 
-
-class RepeaterGraph(Graph):
+class RepeaterGraph(nx.DiGraph):
     """Single repeater."""
 
     def __init__(self):
+        super().__init__()
         logger.debug("Creating repeater graph.")
-        self.nodes = {0, 1, 2}
-        self.edges = {0: {1: 1}, 1: {0: 1, 2: 1}, 2: {1: 1}}
+        edges = {0: {1: 1}, 1: {0: 1, 2: 1}, 2: {1: 1}}
+        temp = nx.DiGraph(edges)
+        self.add_edges_from(temp.edges)
 
-
-class TriangleGraph(Graph):
+class TriangleGraph(nx.DiGraph):
     """Three fully connected nodes."""
 
     def __init__(self):
+        super().__init__()
         logger.debug("Creating triangle graph.")
-        self.nodes = {0, 1, 2}
-        self.edges = {0: {1: 1, 2: 1}, 1: {0: 1, 2: 1}, 2: {0: 1, 1: 1}}
+        edges = {0: {1: 1, 2: 1}, 1: {0: 1, 2: 1}, 2: {0: 1, 1: 1}}
+        temp = nx.DiGraph(edges)
+        self.add_edges_from(temp.edges)
