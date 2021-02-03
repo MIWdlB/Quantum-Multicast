52b2bfd5669992d83aaa52cacef5060f669b9166
Michael Williams <ucapiwi@ucl.ac.uk>
2021-02-03 00:00:04 +0000

    merge again?

--- qmulticast/utils/__init__.py
+++ qmulticast/utils/__init__.py
@@ -1,12 +1,8 @@
 """Init utils modules"""
 
 from .bipartite_network import create_bipartite_network
-<<<<<<< HEAD
 from .functions import fidelity_from_node, gen_GHZ_ket, log_entanglement_rate
 from .graph import Graph
-=======
-from .functions import gen_GHZ_ket
->>>>>>> dd1a4d6867e757235f9f0128c57749ea04218a2b
 from .graphlibrary import ButterflyGraph, RepeaterGraph, TwinGraph
 
 __all__ = [
