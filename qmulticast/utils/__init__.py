dba1c838d9d5e48b121820fc8c82821e6f8f149c
Evan Sutcliffe <evansutcliffe@gmail.com>
2021-02-03 19:46:16 +0000

    update innit

--- qmulticast/utils/__init__.py
+++ qmulticast/utils/__init__.py
@@ -2,7 +2,7 @@
 
 from .bipartite_network import create_bipartite_network
 from .multipartite_network import create_multipartite_network
-from .functions import gen_GHZ_ket
+from .functions import fidelity_from_node, gen_GHZ_ket, log_entanglement_rate
 from .graphlibrary import ButterflyGraph, RepeaterGraph, TwinGraph
 
 __all__ = [
@@ -10,6 +10,8 @@ __all__ = [
     TwinGraph,
     RepeaterGraph,
     gen_GHZ_ket,
+    fidelity_from_node,
+    log_entanglement_rate,
     create_bipartite_network,
     create_multipartite_network,
 ]
